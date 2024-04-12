4d5f0407d9a5039c4548cc3c70dcb767d949b190
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-12 18:26:53 +0900

    7.9 Styles _ summary ...로 수정하기

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,7 @@ function Movie({id, coverImg, title, summary, genres}){
           {/* <img src={medium_cover_image} /> */}
           <img src={coverImg} alt="title" />
           <Link to={`/movie/${id}`}>{title}</Link>
-          <p>{summary}</p>
+          <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
           <ul>
             {genres.map(g => (
             <li key={g}>{g}</li>
